275a5a99a9df71ea40a5bc44208030f8b17595cd
yfhu <879649442@qq.com>
2021-08-26 14:57:14 +0800

    更新

--- vue.config.js
+++ vue.config.js
@@ -19,8 +19,8 @@ module.exports = {
   pages: {
     index: {
       entry: "examples/main.js", // 入口
-      template: "public/index.html", // 模板
-      filename: "index.html", // 输出文件
+      // template: "public/index.html", // 模板
+      // filename: "index.html", // 输出文件
     },
   },
   productionSourceMap: false,
